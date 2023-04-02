e6676b7c2b5d163d903920f6239d938d1be836e5
Michael Hudson <mnkotagu@gmail.com>
2023-04-02 16:24:06 +0300

    Implement using factors program using Python

---
+++ factors.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: factors <file>")
+    sys.exit()
+
+file_path = sys.argv[1]
+
+file = open(file_path, 'r')
+lines = file.readlines()
+
+for line in lines:
+    num = int(line)
+    flag = False
+    for i in range(2, num // 2 + 1):
+        if num % i == 0:
+            print(f"{num}={i}*{num // i}")
+            flag = True
+            break
